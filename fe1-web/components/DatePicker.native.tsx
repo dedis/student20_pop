dab48747d710f5aec45a31ca33b6e1a98e67a290
Adalsteinn Jonsson <adalsteini@gmail.com>
2021-04-09 16:33:21 +0200

    Added template for native datepicker but can't get the app to work on mobile, due to other bugs in the application

--- fe1-web/components/DatePicker.native.tsx
+++ fe1-web/components/DatePicker.native.tsx
@@ -1,19 +1,53 @@
-import React from 'react';
+import React, { useState } from 'react';
 import PropTypes from 'prop-types';
 import DateTimePicker from '@react-native-community/datetimepicker';
+import { View, Button, Platform } from 'react-native';
 
 const DatePicker = (props: IPropTypes) => {
   const { selected } = props;
   const { onChange } = props;
   const initialEndDate = new Date();
 
+  const [date, setDate] = useState(new Date(1598051730000));
+  const [mode, setMode] = useState('date');
+  const [show, setShow] = useState(false);
+
+  const changeDate = (event, selectedDate) => {
+    const currentDate = selectedDate || date;
+    setShow(Platform.OS === 'ios');
+    setDate(currentDate);
+  };
+
+  const showMode = (currentMode) => {
+    setShow(true);
+    setMode(currentMode);
+  };
+
+  const showDatepicker = () => {
+    showMode('date');
+  };
+
+  const showTimepicker = () => {
+    showMode('time');
+  };
+
+
   return (
-    <DateTimePicker
-      value={initialEndDate}
-      mode="default"
-      display="default"
-      onChange={(date: any) => onChange(date)}
-    />
+    <View>
+      <View>
+        <Button onPress={showDatepicker} title="Pick Date" />
+      </View>
+      <View>
+        <Button onPress={showTimepicker} title="Show time picker!" />
+      </View>
+      <DateTimePicker
+        value={initialEndDate}
+        mode="default"
+        display="default"
+        onChange={(date: any) => changeDate(date)}
+      />
+    </View>
+
   );
 };
 
